3fba85e222b45810f6989ed85b1034e5ac5a3e3b
virenb <virenb@gmail.com>
2020-10-30 17:02:06 -0400

    Layout

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>About!</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from '../../layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>Home</BlogLayout>
+}
+
+export default HomePage
